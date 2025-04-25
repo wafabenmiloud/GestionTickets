390b641db8802d37c059d36cd034a9af1f090b49
Wafa Ben Miloud <Wafabenmiloud@gmail.com>
2025-04-25 18:48:07 +0100

    25.04

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getTickets } from "../services/ticketService";
+import { Link } from 'react-router-dom';
 
 export default function UserDashboard() {
   const [tickets, setTickets] = useState([]);
